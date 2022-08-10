64723a50509c8d3499fac37ca67f2db09ce61460
Ted Dino <ted23@tutanota.com>
2022-08-10 17:39:26 +0800

    add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#333333",
+        secondary: "#4F4F4F",
+        accent: "#BDBDBD",
+        btnPrimary: "#3DB46D",
+        btnSecondary: "#EB5757",
+      },
+    },
   },
   plugins: [],
-}
+};
