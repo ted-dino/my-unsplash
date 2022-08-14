c87ba362edbce24ade75a8985ff36e32de7bc06f
Ted Dino <ted23@tutanota.com>
2022-08-14 19:17:32 +0800

    add create and delete api for post

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -2,7 +2,43 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "../../utils/supabaseClient";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  let { data: posts, error } = await supabase.from("posts").select("*");
+  const { method } = req;
 
-  res.status(200).json(posts);
+  // yeah, i know. i should've used switch case or something
+  if (method === "GET") {
+    let { data: posts, error } = await supabase
+      .from("posts")
+      .select("*")
+      .order("created_at", { ascending: false });
+
+    if (error) {
+      res.status(500).send({ error });
+      return;
+    }
+
+    res.status(200).json(posts);
+  } else if (method === "POST") {
+    const { link, label } = req.body;
+    const { data, error } = await supabase
+      .from("posts")
+      .insert([{ photo_url: link, label: label }]);
+
+    if (error) {
+      res.status(500).send({ error });
+      return;
+    }
+    res.status(200).json(data);
+  } else if (method === "DELETE") {
+    const { photoID } = req.body;
+    const { data, error } = await supabase
+      .from("posts")
+      .delete()
+      .eq("id", photoID);
+
+    if (error) {
+      res.status(500).send({ error });
+      return;
+    }
+    res.status(200).json(data);
+  }
 };
